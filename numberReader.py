89b27e9966cdf0459785ae4e8d8fcc16a8907c97
nononono-uec <93241583+nononono-uec@users.noreply.github.com>
2024-03-17 17:12:42 +0900

    二値化にも成功。けど無理

--- numberReader.py
+++ numberReader.py
@@ -4,11 +4,13 @@ import pyocr
 import pyocr.builders
 import sys
 from glob import glob
+import numpy as np
+from matplotlib import pylab as plt
 
 # Tesseract実行ファイルのパス
 pyocr.tesseract.TESSERACT_CMD = r'C:\Users\Amano\Documents\home\Python\AutoDistribute\Tesseract-OCR\tesseract.exe'
 # 切り抜く長方形の座標。左上が原点で、左、上、右、下の順。単位はミリ
-kirinuki = [99, 21, 127, 33]
+kirinuki = [98, 20, 126, 31]
 # ミリからピクセルへ変換する用の比例定数
 dpi = 200 # スキャナによって変える
 left, upper, right, lowwer = [dpi * i / 25.4 for i in kirinuki]
@@ -32,11 +34,15 @@ for file_path in file_list:
     # 画像を拡大
     k = 3 # 拡大率
     max_medals_img = max_medals_img.resize((max_medals_img.width * k, max_medals_img.height * k), Image.LANCZOS)
+    # 画像を二値化
+    max_medals_img = max_medals_img.convert("L") # グレイスケールに変換
+    threshold = 220 # 二値化の閾値
+    binary_image = max_medals_img.point(lambda x: 255 if x > threshold else 0, '1')
+    max_medals_img = binary_image
+
     # デバッグ用
     max_medals_img.save("numHoge.png")
 
-    # 背景色と文字色を反転（白文字→黒文字へ変換）
-    # max_medals_img = ImageOps.invert(max_medals_img.convert('RGB'))
     # OCR
     max_medals = tool.image_to_string(max_medals_img, lang='eng', builder=pyocr.builders.DigitBuilder(tesseract_layout=8))
     # 数値以外の文字を除去
